7831cadc4a4f01b25714137678b224a25c590ce4
Moustapha Ebnou <ebnou@Moustaphas-MacBook-Pro.local>
2018-11-14 19:32:12 +0100

    adding files

---
+++ model.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 14:42:03 2018
+
+@author: ebnou
+"""
+import keras
+from keras.models import Sequential , load_model , Model
+from keras.layers import Dense , LSTM, Dropout , Conv1D , MaxPooling1D , Input, Reshape , Masking , TimeDistributed
+from keras.layers import Concatenate , BatchNormalization , Bidirectional , Activation , GlobalMaxPooling1D
+from keras.preprocessing.sequence import TimeseriesGenerator , pad_sequences
+from keras.callbacks import History , ModelCheckpoint
+from keras.preprocessing.text import Tokenizer
+from keras.constraints import maxnorm
+from keras.regularizers import l1_l2
+
+from keras.layers import Embedding
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error , precision_score , accuracy_score
+from sklearn.metrics import recall_score , confusion_matrix, roc_curve, auc
+
+"""
+PARAMETERS:
+X : Features Matrix 
+Y : Target Matrix (Binary : Classification)
+-----
+cnn_layers = list / Size of each cnn layer in the architechture
+cnn_kernels = list / Kernel of each cnn layer in the architechture
+cnn_dropout = list / dropout rate of each cnn layer
+-----
+lstm_layers = list / Size of each Bidirectional LSTM.
+lstm_dropout = list / Dropout rate of each Bidirectionnal LSTM.
+-----
+Vector_size : int / Size of the first Fully connected after the concatenation of the 
+              Vectors. Will be the feature vector that the model extract.
+-----
+Training parameters:
+    lr : learning rate.
+    epochs : Number of epochs.
+    batcj_size : batch_size.
+    ntest_sers : Number of sequences that we use for validation.
+-----
+Verbose : Wether or not print the summary.
+"""
+    
+cnn_layers = [32 , 64 , 128]
+cnn_kernels = [3 , 3 , 3]
+cnn_dropout = [.5 , .5 , .5]
+lstm_layers = [128] 
+lstm_dropout = [.5]
+vector_size = 128
+lr = 0.001
+epochs = 20
+batch_size = 64
+ntest_sers = 1000
+verbose = True
+    
+
+######### Modèle :    
+inp = Input(shape = (X.shape[1],X.shape[2]))
+
+
+# Convolutional Part
+
+x1 = inp
+# We apply the convolutional Filters.
+for i in range(len(cnn_layers)):
+    x1 = Conv1D(cnn_layers[i] , cnn_kernels[i], padding = 'same')(x1)
+    x1 = BatchNormalization()(x1)
+    x1 = Activation('relu')(x1)
+    x1 = Dropout(cnn_dropout[0])(x1)
+# Global Pooling.
+x1 = GlobalMaxPooling1D()(x1)
+
+
+
+
+# BI lSTM part :
+x2 = Bidirectional( LSTM(lstm_layers[0] , return_sequences = False))(inp)
+x2 = Dropout(lstm_dropout[0])(x2)
+
+# Concatenation:
+x = Concatenate(axis = -1)([x1 , x2])
+
+# Dense layer : Feature Vector.
+x = Dense(vector_size , activation = 'relu')(x)
+
+# Output 
+x = Dense(Y.shape[1] , activation = 'softmax')(x)
+
+model = Model(inputs=inp, outputs=x)
+
+#####################
+
+# Model Summary
+if verbose : print(model.summary())
+
+# Optimize & Compilation.
+Nadam = keras.optimizers.Nadam(lr = lr , beta_1=0.9, beta_2=0.999, epsilon=1e-08)#, schedule_decay=0.0004)
+model.compile(loss='categorical_crossentropy', optimizer= Nadam , metrics = ['accuracy'])
+
+# We fit the model.
+checkpoint = ModelCheckpoint('model' , save_best_only=True) # Save the best model.
+
+model.fit(X, Y, epochs= epochs, batch_size=batch_size, validation_split = ntest_sers/X.shape[0] 
+          , callbacks = [checkpoint])
---
+++ preprocessing.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 19:21:27 2018
+
+@author: ebnou
+"""
+
+%%capture
+%pylab inline
+
+import pandas as pd
+import numpy as np
+
+from collections import Counter
+
+import unidecode
+import re, string, timeit
+from nltk.corpus import stopwords
+
+import seaborn as sns
+sns.set()
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from copy import deepcopy
+import io
+
+data = pd.read_csv('taonews.csv', sep = ';')
+data_count = data[["domain","text"]].groupby("domain").count()
+domain_names = set(data_count[data_count['text']>500].index)
+new_data = data.loc[data['domain'].isin(domain_names)]
+new_data = new_data.sample(n=5000)
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+tokenizer = Tokenizer(num_words=100000,filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~0123456789\n\xa0')
+tokenizer.fit_on_texts(new_data.text)
+sequences = tokenizer.texts_to_sequences(new_data.text)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+
+
+#Word Embedding
+
+# -*- coding: utf-8 -*-
+embeddings_index = {}
+f = open('glove.6B.100d.txt', encoding="utf8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index)) 
+
+word_index_key = dict(zip(word_index.values() , word_index.keys()))
+
+
+def convert_vector(e , d = 100):
+    try:
+        return embeddings_index[word_index_key[e]]
+    except:
+        return np.zeros(d)
+
+
+emb_sequences = [ [ convert_vector(e) for e in l ] for l in sequences]
+
+# We pad the sequences.
+fill = np.zeros(100)
+max_len = 800
+
+for l in emb_sequences:
+    while len(l) < max_len:
+        l.append(fill)
+        
+        
+X = pad_sequences(emb_sequences, maxlen=800)
+
+Y = pd.get_dummies(new_data.domain).values
+print('Shape of data tensor:', X.shape)
+print('Shape of label tensor:', Y.shape)
+
